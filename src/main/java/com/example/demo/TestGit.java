f937f224483ed05e269a1c16f17d6f8180ea47dd
hoangtrung <trung.hoangquoc203@gmail.com>
2023-04-11 01:49:50 +0700

    A2

--- src/main/java/com/example/demo/TestGit.java
+++ src/main/java/com/example/demo/TestGit.java
@@ -5,6 +5,10 @@ public class TestGit {
     public static void main(String[] args) {
 
         String a = new String();
+        String b = new String();
+        String c = new String();
 
+        String d = new String();
+        String e = new String();
     }
 }

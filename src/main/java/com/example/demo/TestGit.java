347991712626f475c679b15b40d2643aa4491d98
trung hoang <trung.hoangquoc203@gmail.com>
2023-04-11 01:26:41 +0700

    Test rebase 3 commit
    This reverts commit 6e0650ff45d2b53ca77993e71d3f1a0efed96cef.
    
    This reverts commit 69a057b9abf2930928e6f77d7ce9d5372316df36.

---
+++ src/main/java/com/example/demo/TestGit.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+
+        String a = new String();
+        String b = new String();
+        String c = new String();
+
+        String d = new String();
+        String e = new String();
+    }
+
+    public String testGit(){
+
+        String a = new String();
+        String b = new  String();
+
+        return "";
+    }
+}
